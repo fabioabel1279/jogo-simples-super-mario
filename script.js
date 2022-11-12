ef3efae43d72d867c230146c940b651172327c24
Fabio Nonato <113532359+fabioabel1279@users.noreply.github.com>
2022-11-11 23:09:41 -0300

    Add files via upload
    Continuação do jogo. Porém ainda não finalizado.

--- script.js
+++ script.js
@@ -9,12 +9,26 @@ setTimeout(() => {
 }
 
 const loop = setInterval(() => {
+    console.log('loop')
 
 const pipePosition = pipe.offsetLeft;
-if (pipePosition ≤ 120) {
+const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
+
+console.log(mario); 
+
+if (pipePosition < 120 && pipePosition > 0 && marioPosition < 80) {
 
     pipe.style.animation = 'none';
     pipe.style.left = '${pipePosition}px';
+
+    mario.style.animation = 'none';
+    mario.style.bottom = '${marioPosition}px';
+
+    mario.src = './images/game-over.png';
+    mario.style.width = '75px'
+    mario.style.marginLeft = '50px'
+
+    clearInterval(loop);
      
 }
 
